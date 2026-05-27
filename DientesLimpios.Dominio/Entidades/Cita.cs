1bed4a969e16bcc2d72160027ac8b2a4d4534c57
Westly Meza <westlymeza@gmail.com>
2026-05-27 01:22:35 -0600

    Creando Entidades

---
+++ DientesLimpios.Dominio/Entidades/Cita.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DientesLimpios.Dominio.Entidades
+{
+    public class Cita
+    {
+        public Guid Id { get; private set; }
+        public Guid PacienteId { get; private set; }
+        public Guid DentistaId { get; private set; }
+        public Guid ConsultorioId { get; private set; }
+        public Enums.EstadoCita Estado { get; private set; }
+        public DateTime FechaInicio { get; private set; }
+        public DateTime FechaFin { get; private set; }
+        public Paciente? Paciente { get; private set; }
+        public Dentista? Dentista { get; private set; }
+        public Consultorio? Consultorio { get; private set; }
+
+    }
+}
---
+++ DientesLimpios.Dominio/Entidades/Dentista.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DientesLimpios.Dominio.Entidades
+{
+    public class Dentista
+    {
+        public Guid Id { get; private set; }
+        public string Nombre { get; private set; } = null!;
+        public string Email { get; private set; } = null!;
+    }
+}
