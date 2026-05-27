0ec280041f1732a16d2f0dc90bb3dad1a4e6bfc6
Westly Meza <westlymeza@gmail.com>
2026-05-27 11:34:48 -0600

    Agregando comandos

---
+++ DientesLimpios.Aplicacion/CasosDeUso/Consultorios/Comandos/CrearConsultorio/CasoDeUsoCrearConsultorio.cs
@@ -0,0 +1,44 @@
+﻿using DientesLimpios.Aplicacion.Contratos.Persistencia;
+using DientesLimpios.Aplicacion.Contratos.Repositorios;
+using DientesLimpios.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DientesLimpios.Aplicacion.CasosDeUso.Consultorios.Comandos.CrearConsultorio
+{
+    public class CasoDeUsoCrearConsultorio
+    {
+        // Convención, tendremos un método llamado "Ejecutar" que va a recibir un comando y va a devolver un resultado
+
+        private readonly IRepositorioConsultorios repositorio;
+        private readonly IUnidadDeTrabajo unidadDeTrabajo;
+
+        public CasoDeUsoCrearConsultorio(IRepositorioConsultorios repositorio, IUnidadDeTrabajo unidadDeTrabajo)
+        {
+            this.repositorio = repositorio;
+            this.unidadDeTrabajo = unidadDeTrabajo;
+        }
+
+        public async Task<Guid> Handle(ComandoCrearConsultorio comando) 
+        {
+            // orquestamos las acciones a realizar
+            var consultorio = new Consultorio(comando.Nombre);
+
+            try
+            {
+                var respuesta = await repositorio.Agregar(consultorio);
+                await this.unidadDeTrabajo.Persistir();
+                return respuesta.Id;
+            }
+            catch (Exception)
+            {
+                await this.unidadDeTrabajo.Reversar();
+                throw;
+            }
+            
+        }
+    }
+}
