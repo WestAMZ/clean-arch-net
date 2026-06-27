3681ba4752803fec5a5866779d2f18ffbd45c398
Westly Meza <westlymeza@gmail.com>
2026-06-26 21:26:37 -0600

    Agregando caso de uso de consulta

---
+++ DientesLimpios.Pruebas/Aplicacion/CasosDeUso/Consultorios/CasoDeUsoObtenerConsultorioTests.cs
@@ -0,0 +1,65 @@
+﻿using DientesLimpios.Aplicacion.CasosDeUso.Consultorios.Consultas.ObtenerDetalleConsultorio;
+using DientesLimpios.Aplicacion.Contratos.Repositorios;
+using DientesLimpios.Aplicacion.Excepciones;
+using DientesLimpios.Dominio.Entidades;
+using NSubstitute;
+using NSubstitute.ReturnsExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DientesLimpios.Pruebas.Aplicacion.CasosDeUso.Consultorios
+{
+    [TestClass]
+    public class CasoDeUsoObtenerConsultorioTests
+    {
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+        private IRepositorioConsultorios repositorio;
+        private CasoDeUsoObtenerDetalleConsultorio casoDeUso;
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+
+        [TestInitialize]
+        public void Inicializar()
+        {
+            repositorio = Substitute.For<IRepositorioConsultorios>();
+            casoDeUso = new CasoDeUsoObtenerDetalleConsultorio(repositorio);
+        }
+
+        [TestMethod]
+        public async Task Handle_ConsultorioExiste_RetornaDTO()
+        {
+            // Preparación
+
+            var consultorio = new Consultorio("Consultorio A");
+            var id = consultorio.Id;
+            var consulta = new ConsultaObtenerDetalleConsultorio { Id = id };
+
+            repositorio.ObtenerPorId(id).Returns(consultorio);
+
+            // Prueba
+            var resultado = await casoDeUso.Handle(consulta);
+            
+            // Verificación
+            Assert.IsNotNull(resultado);
+            Assert.AreEqual(id, resultado.Id);
+            Assert.AreEqual(consultorio.Nombre, resultado.Nombre);
+        }
+
+        [TestMethod]
+        public async Task Handle_ConsultorioNoExiste_LanzaExcepcionNoEncontrado()
+        {
+            // Preparación
+            var id = Guid.NewGuid();
+            var consulta = new ConsultaObtenerDetalleConsultorio { Id = id };
+
+            repositorio.ObtenerPorId(id).ReturnsNull();
+            // Prueba y verificación
+            await Assert.ThrowsExceptionAsync<ExcepcionNoEncontrado>(async () =>
+            {
+                await casoDeUso.Handle(consulta);
+            });
+        }
+    }
+}
