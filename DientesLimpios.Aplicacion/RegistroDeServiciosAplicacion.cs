7f011c5ec69f09802303b7e9688c7b3baeb8443b
Westly Meza <westlymeza@gmail.com>
2026-06-27 23:06:27 -0600

    Registro de servicios de capa de aplicación

---
+++ DientesLimpios.Aplicacion/RegistroDeServiciosAplicacion.cs
@@ -0,0 +1,24 @@
+﻿using DientesLimpios.Aplicacion.CasosDeUso.Consultorios.Comandos.CrearConsultorio;
+using DientesLimpios.Aplicacion.CasosDeUso.Consultorios.Consultas.ObtenerDetalleConsultorio;
+using DientesLimpios.Aplicacion.Utilidades.Mediador;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DientesLimpios.Aplicacion
+{
+    public static class RegistroDeServiciosAplicacion
+    {
+        public static IServiceCollection AgregarServiciosDeAplicacion( this IServiceCollection services) 
+        {
+            services.AddTransient<IMediator, MediadorSimple>();
+            services.AddScoped<IRequestHandler<ComandoCrearConsultorio,Guid>, CasoDeUsoCrearConsultorio>();
+            services.AddScoped<IRequestHandler<ConsultaObtenerDetalleConsultorio, ConsultorioDetalleDTO>, CasoDeUsoObtenerDetalleConsultorio>();
+
+            return services;
+        }
+    }
+}
