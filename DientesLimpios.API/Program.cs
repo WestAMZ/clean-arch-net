6e853088370e42af9b82aa0d59ef2be23131cef3
Westly Meza <westlymeza@gmail.com>
2026-06-28 16:57:48 -0600

    Creando proyecto api y creando primera migración EF

---
+++ DientesLimpios.API/Program.cs
@@ -0,0 +1,25 @@
+using DientesLimpios.Aplicacion;
+using DientesLimpios.Persistencia;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// Llamando funciones para registrar servicios
+
+builder.Services.AgregarServiciosDeAplicacion();
+builder.Services.AgregarServiciosDePersistencia();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
