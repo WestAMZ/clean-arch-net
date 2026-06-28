28a3091558135c79fea202789a83db7c9ecbe5ee
Westly Meza <westlymeza@gmail.com>
2026-06-27 23:22:13 -0600

    Agregando proyecto de infraestructura para persistencia

---
+++ DientesLimpios.Persistencia/DientesLimpiosDbContext.cs
@@ -0,0 +1,22 @@
+﻿using DientesLimpios.Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DientesLimpios.Persistencia
+{
+    public class DientesLimpiosDbContext : DbContext
+    {
+        public DientesLimpiosDbContext(DbContextOptions<DientesLimpiosDbContext> options) : base(options)
+        {
+        }
+
+        protected DientesLimpiosDbContext()
+        {
+        }
+        public DbSet<Consultorio> Consultorios { get; set; }
+    }
+}
