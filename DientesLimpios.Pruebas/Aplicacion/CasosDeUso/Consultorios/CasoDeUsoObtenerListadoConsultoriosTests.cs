17fe667bc3c80ddc2296a753df864e7ce787ca75
Westly Meza <westlymeza@gmail.com>
2026-07-02 02:11:16 -0600

    Agregando pruebas, para caso de uso listar consultorios

---
+++ DientesLimpios.Pruebas/Aplicacion/CasosDeUso/Consultorios/CasoDeUsoObtenerListadoConsultoriosTests.cs
@@ -0,0 +1,57 @@
+﻿using DientesLimpios.Aplicacion.CasosDeUso.Consultorios.Consultas.ObtenerListadoConsultorios;
+using DientesLimpios.Aplicacion.Contratos.Repositorios;
+using DientesLimpios.Dominio.Entidades;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DientesLimpios.Pruebas.Aplicacion.CasosDeUso.Consultorios
+{
+    [TestClass]
+    public class CasoDeUsoObtenerListadoConsultoriosTests
+    {
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+        private IRepositorioConsultorios repositorio;
+        private CasoDeUsoObtenerListadoConsultorios casoDeUso;
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+
+        [TestInitialize]
+        public void Setup()
+        {
+            repositorio = NSubstitute.Substitute.For<IRepositorioConsultorios>();
+            casoDeUso = new CasoDeUsoObtenerListadoConsultorios(repositorio);
+        }
+        [TestMethod]
+        public async Task Handle_CuandoHayConsultorios_RetornarListaDeConsultorioListadoDTO()
+        {
+            var consultorios = new List<Consultorio>
+            {
+                new Consultorio ("Consultorio A"),
+                new Consultorio ("Consultorio B")
+            };
+
+            repositorio.ObtenerTodos().Returns(consultorios);
+
+            var esperado = consultorios.Select(c => new ConsultorioListadoDTO
+            {
+                Id = c.Id,
+                Nombre = c.Nombre
+            }).ToList();
+            
+            var resultado = await casoDeUso.Handle(new ConsultaObtenerListadoConsultorios());
+            
+
+            Assert.IsNotNull(resultado);
+            Assert.AreEqual(esperado.Count, resultado.Count);
+            for (int i = 0; i < esperado.Count; i++)
+            {
+                Assert.AreEqual(esperado[i].Id, resultado[i].Id);
+                Assert.AreEqual(esperado[i].Nombre, resultado[i].Nombre);
+            }
+            
+        }
+    }
+}
