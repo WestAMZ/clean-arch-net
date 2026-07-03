a25f2ecc4a02eff0c7c5b07212d7ae2e80aa3d83
Westly Meza <westlymeza@gmail.com>
2026-07-03 00:30:11 -0600

    Modificando mediador para casos de uso que no retornan nada

--- DientesLimpios.Aplicacion/Utilidades/Mediador/MediadorSimple.cs
+++ DientesLimpios.Aplicacion/Utilidades/Mediador/MediadorSimple.cs
@@ -19,26 +19,7 @@ namespace DientesLimpios.Aplicacion.Utilidades.Mediador
         }
         public async Task<TResponse> Send<TResponse>(IRequest<TResponse> request)
         {
-            var tipoValidador = typeof(IValidator<>).MakeGenericType(request.GetType());
-
-            var validador = serviceProvider.GetService(tipoValidador);
-
-            if (validador is not null) 
-            {
-                var metodoValidar = tipoValidador.GetMethod("ValidateAsync");
-                var tareaValidar = (Task)metodoValidar!.Invoke(validador, new object[] { request, 
-                    CancellationToken.None })!;
-
-                await tareaValidar.ConfigureAwait(false);
-
-                var resultado = tareaValidar.GetType().GetProperty("Result");
-                var validationResult = (ValidationResult)resultado!.GetValue(tareaValidar)!;
-
-                if (!validationResult.IsValid) 
-                {
-                    throw new ExcepcionDeValidacion(validationResult);
-                }
-            }
+            await RealizarValidaciones(request);
 
             var tipoCasoDeUso = typeof(IRequestHandler<,>)
                 .MakeGenericType(request.GetType(), typeof(TResponse));
@@ -52,6 +33,48 @@ namespace DientesLimpios.Aplicacion.Utilidades.Mediador
 
             var metodo = tipoCasoDeUso.GetMethod("Handle")!;
             return await (Task<TResponse>) metodo.Invoke(casoDeUso, new object[] { request })!;
+        }
+
+        public async Task Send(IRequest request)
+        {
+            await RealizarValidaciones(request);
+
+            var tipoCasoDeUso = typeof(IRequestHandler<>)
+                .MakeGenericType(request.GetType());
+
+            var casoDeUso = serviceProvider.GetService(tipoCasoDeUso);
+
+            if(casoDeUso is null) 
+            {
+                throw new ExcepcionDeMediador($"No se ecnontró un handler para {request.GetType().Name}");
+            }
+
+            var metodo = tipoCasoDeUso.GetMethod("Handle")!;
+            await (Task)metodo.Invoke(casoDeUso, new object[] { request })!;
+        }
+
+        private async Task RealizarValidaciones(object request)
+        {
+            var tipoValidador = typeof(IValidator<>).MakeGenericType(request.GetType());
+
+            var validador = serviceProvider.GetService(tipoValidador);
+
+            if (validador is not null)
+            {
+                var metodoValidar = tipoValidador.GetMethod("ValidateAsync");
+                var tareaValidar = (Task)metodoValidar!.Invoke(validador, new object[] { request,
+                    CancellationToken.None })!;
+
+                await tareaValidar.ConfigureAwait(false);
+
+                var resultado = tareaValidar.GetType().GetProperty("Result");
+                var validationResult = (ValidationResult)resultado!.GetValue(tareaValidar)!;
+
+                if (!validationResult.IsValid)
+                {
+                    throw new ExcepcionDeValidacion(validationResult);
+                }
+            }
         }
     }
 }
